4a653b3dbd8399dea071e818a9230d160568a1a3
Aleksandr <baykov-1988@mail.ru>
2024-10-15 20:56:45 +0300

    modified:   src/main.js modified:   src/presenters/film-presenter.js modified:   src/views/users-rang-view.js

--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@
 import BoardPresenter from './presenters/film-presenter.js';
 import {render} from './render.js';
 import FiltersView from './views/filters-view.js';
+import UsersRankView from './views/users-rang-view.js';
 const siteMainElement = document.querySelector('.main');
-//const siteHeaderElement = siteMainElement.querySelector('.header');
-render(new FiltersView(), siteMainElement);
+const siteHeaderElement = document.querySelector('.header');
 
-const boardPresenter = new BoardPresenter();
-boardPresenter.init(siteMainElement);
+render(new FiltersView(), siteMainElement);
+render(new UsersRankView(), siteHeaderElement);
+
+
+//const boardPresenter = new BoardPresenter();
+//boardPresenter.init(siteMainElement);
+//boardPresenter.init(siteHeaderElement);
--- src/presenters/film-presenter.js
+++ src/presenters/film-presenter.js
@@ -1,12 +1,18 @@
 import {render} from '../render.js';
 import FiltersView from '../views/filters-view.js';
+import UsersRankView from '../views/users-rang-view.js';
 
 export default class BoardPresenter {
   boardComponent = new FiltersView();
+  headerComponent = new UsersRankView();
+
   init = (boardContainer) => {
     this.boardContainer = boardContainer;
 
-    //render(this.boardComponent, this.boardContainer);
+
+    render(this.headerComponent, this.boardContainer);
+    render(new UsersRankView(), this.headerComponent.getElement());
+    render(this.boardComponent, this.boardContainer);
     render(new FiltersView(), this.boardComponent.getElement());
   };
 }
