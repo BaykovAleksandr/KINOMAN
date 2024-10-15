4d970b39b7359656065295ad43ebd163cc445204
Aleksandr <baykov-1988@mail.ru>
2024-10-15 23:28:33 +0300

    modified:   src/main.js modified:   src/views/film-list-view.js

--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
 
 //import BoardPresenter from './presenters/film-presenter.js';
 import {render} from './render.js';
+import ListView from './views/film-list-view.js';
 import FiltersView from './views/filters-view.js';
 import FooterStatisticView from './views/footer-statistic-view.js';
 import UsersRankView from './views/header-profile-view.js';
 const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = document.querySelector('.header');
 const siteFooterElememt = document.querySelector('.footer');
+const siteFilmListElement = document.querySelector('.films');
 
 render(new FiltersView(), siteMainElement);
 render(new UsersRankView(), siteHeaderElement);
+render(new ListView(), siteMainElement);
 render(new FooterStatisticView(), siteFooterElememt);
 //const boardPresenter = new BoardPresenter();
 //boardPresenter.init(siteMainElement);
