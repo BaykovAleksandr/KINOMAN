bfa2a80df775a68b0ac70b0c9aa3d0be2e71082a
Aleksandr <baykov-1988@mail.ru>
2024-10-16 22:32:45 +0300

    modified:   src/main.js modified:   src/presenters/film-presenter.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 
-//import BoardPresenter from './presenters/film-presenter.js';
+import BoardPresenter from './presenters/film-presenter.js';
 import {render, RenderPosition} from './render.js';
 import ListView from './views/film-list-view.js';
 import FilmsNavigationView from './views/film-navigation-view.js';
@@ -11,11 +11,12 @@ const siteHeaderElement = document.querySelector('.header');
 const siteFooterElememt = document.querySelector('.footer');
 //const siteFilmListElement = document.querySelector('.films');
 
-render(new FiltersView(), siteMainElement);
-render(new UsersRankView(), siteHeaderElement);
-render(new ListView(), siteMainElement);
-render(new FooterStatisticView(), siteFooterElememt);
-render(new FilmsNavigationView(), siteMainElement, RenderPosition.BEFOREBEGIN);
-//const boardPresenter = new BoardPresenter();
+// render(new FiltersView(), siteMainElement);
+// render(new UsersRankView(), siteHeaderElement);
+// render(new ListView(), siteMainElement);
+// render(new FooterStatisticView(), siteFooterElememt);
+// render(new FilmsNavigationView(), siteMainElement, RenderPosition.BEFOREBEGIN);
+const boardPresenter = new BoardPresenter();
 //boardPresenter.init(siteMainElement);
-//boardPresenter.init(siteHeaderElement);
+boardPresenter.init(siteHeaderElement);
+
--- src/presenters/film-presenter.js
+++ src/presenters/film-presenter.js
@@ -4,19 +4,20 @@ import FooterStatisticView from '../views/footer-statistic-view.js';
 import UsersRankView from '../views/header-profile-view.js';
 
 export default class BoardPresenter {
-  boardComponent = new FiltersView();
+  mainComponent = new FiltersView();
   headerComponent = new UsersRankView();
   footerComponent = new FooterStatisticView();
 
   init = (boardContainer) => {
     this.boardContainer = boardContainer;
 
-
     render(this.headerComponent, this.boardContainer);
-    render(new UsersRankView(), this.headerComponent.getElement());
-    render(this.boardComponent, this.boardContainer);
-    render(new FiltersView(), this.boardComponent.getElement());
-    render(this.footerComponent, this.boardContainer);
-    render(new FooterStatisticView(), this.boardComponent.getElement());
+
+
+    // render(new FiltersView(), siteMainElement);
+    // render(new UsersRankView(), siteHeaderElement);
+    // render(new ListView(), siteMainElement);
+    // render(new FooterStatisticView(), siteFooterElememt);
+    // render(new FilmsNavigationView(), siteMainElement, RenderPosition.BEFOREBEGIN);
   };
 }
