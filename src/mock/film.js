828eba6a92e9af2eba58315e4bf4be3ccfbb6031
Aleksandr <baykov-1988@mail.ru>
2024-10-18 20:42:03 +0300

    modified:   src/mock/film.js

--- src/mock/film.js
+++ src/mock/film.js
@@ -1,27 +1,5 @@
-const generateFilm = () => ({
-  title: "A Little Pony Without The Carpet",
-  alternative_title: "Laziness Who Sold Themselves",
-  total_rating: 5.3,
-  poster: "images/posters/blue-blazes.jpg",
-  age_rating: 0,
-  director: "Tom Ford",
-  writers: [
-    "Takeshi Kitano"
-  ],
-  actors: [
-    "Morgan Freeman"
-  ],
-  release: {
-    date: "2019-05-11T00:00:00.000Z",
-    release_country: "Finland"
-  },
-  runtime: 77,
-  genre: [
-    "Comedy"
-  ],
-  description: "Oscar-winning film, a war drama about two young people, from the creators of timeless classic \\"Nu, Pogodi!\\" and \\"Alice in Wonderland\\", with the best fight scenes since Bruce Lee."
-});
 
+const getRandomValue = (items) => items[Math.floor(Math.random() * items.length)];
 const titles = [
   'Country On Him',
   'Raiders With The Carpet',
@@ -31,3 +9,30 @@ const titles = [
   'Sugebbrash Trail',
   'The Man With The Golden Arm'
 ];
+
+const generateFilm = () => ({
+
+  title: getRandomValue(titles),
+  alternativeTitle: 'Laziness Who Sold Themselves',
+  totalRating: 5.3,
+  poster: 'images/posters/blue-blazes.jpg',
+  ageRating: 0,
+  director: 'Tom Ford',
+  writers: [
+    'Takeshi Kitano'
+  ],
+  actors: [
+    'Morgan Freeman'
+  ],
+  release: {
+    date: '2019-05-11T00:00:00.000Z',
+    releaseCountry: 'Finland'
+  },
+  runtime: 77,
+  genre: [
+    'Comedy'
+  ],
+  description: 'Oscar-winning film, a war drama about two young people, from the creators of timeless classic \\Nu, Pogodi!\\ and \\Alice in Wonderland\\, with the best fight scenes since Bruce Lee.'
+});
+
+
