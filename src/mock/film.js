1af9a53895df17b5140dcfe0b34d7f82bc670b4f
Aleksandr <baykov-1988@mail.ru>
2024-10-18 21:56:03 +0300

    modified:   src/mock/film.js

--- src/mock/film.js
+++ src/mock/film.js
@@ -1,5 +1,6 @@
 import { getRandomValue } from '../utils.js';
-export { getRandomNumber } from '../utils.js';
+import { getRandomNumber } from '../utils.js';
+import { getRandomFloat } from '../utils.js';
 
 const titles = [
   'Country On Him',
@@ -11,13 +12,17 @@ const titles = [
   'The Man With The Golden Arm'
 ];
 
+const genres = ['kriminal', 'erotic', 'musicl', 'western', 'fantasy', 'action', 'comedy', 'advanture'];
+
+const descriptions = ['Oscar-winning film', 'A war drama about two young people', 'From the creators of timeless classic', 'Nu, Pogodi!', 'And', 'Alice in Wonderland', 'With the best fight scenes since Bruce Lee.'];
+
 const generateFilm = () => ({
 
   title: getRandomValue(titles),
   alternativeTitle: 'Laziness Who Sold Themselves',
-  totalRating: 5.3,
+  totalRating: getRandomFloat(0, 10),
   poster: 'images/posters/blue-blazes.jpg',
-  ageRating: 0,
+  ageRating: getRandomNumber(0, 19),
   director: 'Tom Ford',
   writers: [
     'Takeshi Kitano'
@@ -29,11 +34,11 @@ const generateFilm = () => ({
     date: '2019-05-11T00:00:00.000Z',
     releaseCountry: 'Finland'
   },
-  runtime: 77,
+  runtime: getRandomNumber(70, 181),
   genre: [
     'Comedy'
   ],
-  description: 'Oscar-winning film, a war drama about two young people, from the creators of timeless classic \\Nu, Pogodi!\\ and \\Alice in Wonderland\\, with the best fight scenes since Bruce Lee.'
+  description: descriptions[getRandomNumber(0, descriptions.length)]
 });
 
 
