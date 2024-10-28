8b96fdf7f1bd88ec53fd0adb35333339295026af
Aleksandr <baykov-1988@mail.ru>
2024-10-28 20:31:33 +0300

    modified:   src/presenters/film-presenter.js

--- src/presenters/film-presenter.js
+++ src/presenters/film-presenter.js
@@ -9,22 +9,28 @@ import FilmDetailsView from '../view/film-details-view.js';
 import {render} from '../render.js';
 
 export default class FilmsPresenter {
-  sortComponent = new SortView();
-  filmsComponent = new FilmsView();
-  filmListComponent = new FilmListView();
-  filmListContainerComponent = new FilmListContainerView();
-  filmButtonMoreComponent = new FilmButtonMoreView();
+  #sortComponent = new SortView();
+  #filmsComponent = new FilmsView();
+  #filmListComponent = new FilmListView();
+  #filmListContainerComponent = new FilmListContainerView();
+  #filmButtonMoreComponent = new FilmButtonMoreView();
+
+  #container = null;
+  #filmsModel = null;
+  #commentsModel = null;
+
+  #films = [];
 
   init = (container, filmsModel, commentsModel ) => {
-    this.container = container;
-    this.filmsModel = filmsModel;
-    this.commentsModel = commentsModel;
-    this.films = [...filmsModel.get()];
+    this.#container = container;
+    this.#filmsModel = filmsModel;
+    this.#commentsModel = commentsModel;
+    this.#films = [...this.#filmsModel.get()];
 
-    render(this.sortComponent, this.container);
-    render(this.filmsComponent, this.container);
-    render(this.filmListComponent, this.filmsComponent.getElement());
-    render(this.filmListContainerComponent, this.filmListComponent.getElement());
+    render(this.#sortComponent, this.#container);
+    render(this.#filmsComponent, this.#container);
+    render(this.#filmListComponent, this.#filmsComponent.getElement());
+    render(this.#filmListContainerComponent, this.#filmListComponent.getElement());
 
     for (let i = 0; i < this.films.length; i++) {
       render(new FilmCardView(this.films[i]), this.filmListContainerComponent.getElement());
