afff46bb28c049ac939c9b37bd003eac7454b803
Aleksandr <baykov-1988@mail.ru>
2024-10-28 20:56:22 +0300

    modified:   src/view/film-card-view.js modified:   src/view/film-details-view.js

--- src/view/film-details-view.js
+++ src/view/film-details-view.js
@@ -35,25 +35,30 @@ const createFilmDetailsTemplate = ({filmInfo}, comments) =>
     </section>
  `;
 
-export default class FilmDetailsView {
+ export default class FilmDetailsView {
+  #element = null;
+  #film = null;
+  #comments = null;
+
   constructor(film, comments) {
-    this.film = film;
-    this.comments = comments;
+    this.#film = film;
+    this.#comments = comments;
   }
 
-  getTemplate() {
-    return createFilmDetailsTemplate(this.film, this.comments);
+  get template() {
+    return createFilmDetailsTemplate(this.#film, this.#comments);
   }
 
-  getElement() {
-    if (!this.element) {
-      this.element = createElement(this.getTemplate());
+  get element() {
+    if (!this.#element) {
+      this.#element = createElement(this.template);
     }
 
-    return this.element;
+    return this.#element;
   }
 
   removeElement() {
-    this.element = null;
+    this.#element = null;
   }
 }
+
