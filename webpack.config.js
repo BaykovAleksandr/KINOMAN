6bdfdabca8ba9a8357cb18f0a09fb04b09f6966e
Aleksandr <baykov-1988@mail.ru>
2024-11-01 22:45:47 +0300

    modified:   webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,30 @@
-const path = require("path");
+const path = require('path');
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: './src/main.js',
   output: {
     filename: 'bundle.js',
-    // eslint-disable-next-line no-undef
     path: path.resolve(__dirname, 'build'),
     clean: true,
   },
   devtool: 'source-map',
   plugins: [
     new CopyPlugin({
-      patterns: [{from: 'public'}],
+      patterns: [{ from: 'public' }],
     }),
   ],
   module: {
     rules: [
-      {
-        test:/\.js$/,
-        exclude: /(node_modules)/,
-        use: ['babel-loader']
-      }
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          use: ['babel-loader']
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader']
+        }
     ]
   }
 };
