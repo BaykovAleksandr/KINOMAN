595f9b2599d10bdb535cc023841552831df0468b
Aleksandr <baykov-1988@mail.ru>
2024-11-01 20:27:44 +0300

    modified:   src/mock/comment.js deleted:    src/utils.js new file:   src/utils/common.js new file:   src/utils/film.js modified:   src/view/film-card-info-template.js modified:   src/view/film-details-comments-template.js modified:   src/view/film-details-info-template.js

---
+++ src/const.js
@@ -0,0 +1,11 @@
+const FILM_COUNT = 20;
+
+const FILM_COUNT_PER_STEP = 5;
+
+const EMOTIONS = ['smile', 'sleeping', 'puke', 'angry'];
+
+export {
+  FILM_COUNT,
+  EMOTIONS,
+  FILM_COUNT_PER_STEP
+};
--- src/mock/comment.js
+++ src/mock/comment.js
@@ -1,23 +1,33 @@
-import { getRandomValue, humanizeCommentDueDate } from '../utils';
+import {getRandomInteger, getRandomValue} from '../utils/common.js';
+import {
+  DaysDuration,
+  names,
+  surnames,
+  emotions,
+  comment,
+} from './const.js';
 
-const emotions = ['smile', 'sleeping', 'puke', 'angry'];
+const getDate = () => {
+  const date = new Date();
 
-const authors = ['Ilya O \'Reilly', 'Max Kurchiy', 'Alex Baykov', 'Alan Delon', 'Harry Potter', 'Ilya Chert', 'Kirill Mokevnin', 'Stive Jobs', 'Donald Tramp'];
+  date.setDate(
+    date.getDate() - getRandomInteger(DaysDuration.MIN, DaysDuration.MAX)
+  );
 
-const fishComments = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.'.split('. ');
+  return date.toISOString();
+};
 
-const generateComment = () => (
-  {
-    'author': getRandomValue(authors),
-    'comment': getRandomValue(fishComments),
-    'date': humanizeCommentDueDate('2019-05-11T16:12:32.554Z'),
-    'emotion': getRandomValue(emotions)
-  }
-);
+const generateComment = () => ({
+  author: `${getRandomValue(names)} ${getRandomValue(surnames)}`,
+  comment,
+  date: getDate(),
+  emotion: getRandomValue(emotions),
+});
 
 const getCommentCount = (films) => films.reduce(
   (count, film) => count + film.comments.length, 0
 );
+
 const generateComments = (films) => {
   const commentCount = getCommentCount(films);
 
@@ -31,4 +41,4 @@ const generateComments = (films) => {
   });
 };
 
-export { generateComments };
+export {generateComments};
